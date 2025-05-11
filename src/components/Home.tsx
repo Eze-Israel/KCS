b7d89f72d51fac548683b252315d871ffcd250f4
Eze-Israel <ezeisraeloluoma123@gmail.com>
2025-05-11 17:46:14 +0100

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+      <div>
+        A Government Approved Primary and Secondary School
+        Features
+        3 well built 3 story building sitting on 2 plots of land
+        39 executives classrooms with Air Conditions and Television sets
+        18 toilets for admin offices
+        Swimming pool and cool learning environment
+        120 capacity hall
+        Modern days school furniture
+        With over 180 students secondary and primary
+
+        </div>
+  )
+}
+
+export default Home
